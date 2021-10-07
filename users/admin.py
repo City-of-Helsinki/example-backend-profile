48cc519fca229329567a78fbb047ab9093d3a896
Mikko Keskinen <mikko.keskinen@newthings.co>
2021-10-07 12:43:30 +0300

    Add UserData model
    The model will be used to host all the user's data.

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import User, UserData
 
-admin.site.register(User, UserAdmin)
+
+class UserDataInline(admin.StackedInline):
+    model = UserData
+    can_delete = False
+    verbose_name_plural = "User data"
+
+
+class ExampleUserAdmin(UserAdmin):
+    inlines = (UserDataInline,)
+
+
+admin.site.register(User, ExampleUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,5 +1,11 @@
+from django.db import models
 from helusers.models import AbstractUser
 
 
 class User(AbstractUser):
     pass
+
+
+class UserData(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pet_name = models.CharField(max_length=255, null=True, blank=True)

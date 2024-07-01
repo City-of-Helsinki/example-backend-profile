8ba2d1e7bce159dc50f964433e0263a88ae86254
Juha Louhiranta <juha.louhiranta@anders.fi>
2024-07-01 12:18:33 +0300

    feat: add more information and controls to ExampleUserAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,29 @@
+import json
+from copy import deepcopy
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext_lazy as _
+from social_django.models import UserSocialAuth
 
 from .models import User, UserData
+
+
+class UserSocialAuthInline(admin.StackedInline):
+    model = UserSocialAuth
+    readonly_fields = ["get_extra_data"]
+    exclude = ["extra_data"]
+    extra = 0
+
+    def get_extra_data(self, obj=None):
+        if not obj or not obj.extra_data:
+            return ""
+
+        return mark_safe(f"<pre>{escape(json.dumps(obj.extra_data, indent=4))}</pre>")
+
+    get_extra_data.short_description = _("Extra data")
 
 
 class UserDataInline(admin.StackedInline):
@@ -10,8 +32,19 @@ class UserDataInline(admin.StackedInline):
     verbose_name_plural = "User data"
 
 
+@admin.register(User)
 class ExampleUserAdmin(UserAdmin):
-    inlines = (UserDataInline,)
+    list_display = ("uuid",) + UserAdmin.list_display
+    inlines = (UserDataInline, UserSocialAuthInline)
 
-
-admin.site.register(User, ExampleUserAdmin)
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        if obj:
+            fieldsets = deepcopy(fieldsets)
+            fieldsets[1][1]["fields"] += ("uuid",)
+            permission_fields = list(fieldsets[2][1]["fields"])
+            fieldsets[2][1]["fields"] = tuple(permission_fields) + (
+                "department_name",
+                "ad_groups",
+            )
+        return fieldsets

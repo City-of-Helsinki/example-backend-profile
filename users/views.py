ee80c74ab2b0a77c198315c64fea0d4fbbe5006e
Nico Virkki <nico.virkki@anders.com>
2025-10-27 17:51:49 +0200

    feat: upgrade to django 5.2
    Refs: RATYK-219

--- users/views.py
+++ users/views.py
@@ -27,7 +27,7 @@ class MyUserDataView(RetrieveAPIView, UpdateAPIView):
         try:
             return UserData.objects.get(user=self.request.user)
         except UserData.DoesNotExist:
-            raise NotFound()
+            raise NotFound() from None  # noqa: B904
 
 
 class FillMyBirthday(APIView):

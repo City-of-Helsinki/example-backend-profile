b3fb4b3e4c3915b9165a28c2627dc9e852bdafd1
Mika Hietanen <m.hietanen84@gmail.com>
2026-09-08 17:40:41 +0300

    fix: support uWSGI toggle in Docker Compose
    Select the server based on DEV_SERVER and document config.env setup.
    
    Refs: RATYK-210

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,19 @@
 
 ./manage.py migrate --noinput
 
-./manage.py runserver --insecure 0.0.0.0:8000
+# Match django-environ's boolean parsing (case-insensitive; true/on/ok/y/yes/1 are truthy).
+dev_server_flag=$(printf '%s' "$DEV_SERVER" | tr '[:upper:]' '[:lower:]')
+
+case "$dev_server_flag" in
+    true | on | ok | y | yes | 1)
+        exec ./manage.py runserver --insecure 0.0.0.0:8000
+        ;;
+    *)
+        exec uwsgi \
+            --module example_backend_profile.wsgi:application \
+            --http 0.0.0.0:8000 \
+            --master \
+            --processes 2 \
+            --threads 2
+        ;;
+esac
